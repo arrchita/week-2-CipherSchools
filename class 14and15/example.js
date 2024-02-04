9e670c46fedc3d68eadd3bd19da8a2f55344214b
archita <deepg9997@gmail.com>
2024-02-05 01:05:51 +0530

    class 15 content

---
+++ class 14and15/example.js
@@ -0,0 +1,19 @@
+//strings
+console.log(typeof "it is raining");
+let word="raining";
+console.log(word.toUpperCase());
+console.log(word.toLowerCase());
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(isNaN('five'));
+
+//truthy values: any value except 0 and false
+//falsy values: 0 false ''NaN undefined
+if(hello){
+    console.log("very true!!");
+}else{
+    console.log("soo wrong boohoo!")
+}
+
+
