9ffebffe4a6fe82ac2ef623beaedea9e96e100b3
archita <deepg9997@gmail.com>
2024-02-20 16:00:10 +0530

    class 18

--- class14to19/objects.js
+++ class14to19/objects.js
@@ -10,8 +10,22 @@ let person={
     children:{
         name:'Jay'
     },
-    hobbies:['reading', 'writing', true, 100, null]
-}
+    hobbies:['reading', 'writing', true, 100, null],
+
+    login: function(){
+        console.log(this.name, "user has logged in");
+        return this; //Returning 'this' for method chaining
+    },
+    logout() {
+        console.log('Logout', this.login());
+        return this; // Returning 'this' for method chaining
+    },
+    updateLogin() {
+        this.count++;
+        console.log(this.count);
+        return this; // Returning 'this' for method chaining
+    },
+};
 
 let clickedName='name';
 
@@ -24,3 +38,5 @@ console.log(person['age']);
 console.log(person['children']);
 
 console.log(typeof person.hobbies);
+
+person.login();
